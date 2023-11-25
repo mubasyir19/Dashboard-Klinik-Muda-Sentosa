f52fa171d3a10d1e9715509629541089d9b820fd
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2023-11-25 14:46:56 +0700

    add(home): amount of data

--- app/Home/controller.js
+++ app/Home/controller.js
@@ -1,9 +1,22 @@
+const { Account, Article, Category, Consultation } = require('../../db/models');
+
 module.exports = {
   HomeDashboard: async (req, res) => {
+    const account = await Account.count();
+    const article = await Article.count();
+    const category = await Category.count();
+    const consultation = await Consultation.count({ where: { answer: null } });
+
     try {
       res.render('index', {
         user: req.session.account,
         route: 'Dashboard',
+        count: {
+          account,
+          article,
+          category,
+          consultation,
+        },
       });
     } catch (error) {
       console.log(error);
